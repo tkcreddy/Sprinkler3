5b42d40d293e206681509e1e39cbd8ce9d12ef09
krishnareddy <tkcreddy@yahoo.com>
2019-01-25 15:20:03 -0800

    update few lines of exceptional handling

--- com/aak/modules/runtime/runjobs.py
+++ com/aak/modules/runtime/runjobs.py
@@ -1,8 +1,18 @@
 from com.aak.modules.runtime.runzonejob import *
+import traceback
 
 class runjobs():
     def __init__(self,**data):
         self.jobs_hash = {}
         self.jobs_hash = data
-        for key, value in self.jobs_hash.items():
-            Runzonejob(key,value)
+        try:
+            for key, value in self.jobs_hash.items():
+                Runzonejob(key,value)
+
+        except Exception as ex:
+            traceback.print_exc()
+
+        finally:
+            print("completed the job")
+            pass
+
--- com/aak/modules/runtime/testjob.py
+++ com/aak/modules/runtime/testjob.py
@@ -1,4 +1,3 @@
-from com.aak.modules.runtime.runzonejob import Runzonejob
 from com.aak.modules.runtime.runjobs import *
 
 dct1 = { "1" : 10}
