4c211ff8c56dbe910b78db39ef9d2d4c0a3f2c47
krishnareddy <tkcreddy@yahoo.com>
2019-01-30 18:34:35 -0800

    Changes to runzone job

--- com/aak/modules/runtime/runzonejob.py
+++ com/aak/modules/runtime/runzonejob.py
@@ -1,4 +1,4 @@
-from com.aak.modules.config.configread import configread
+from com.aak.modules.config.configRead import Configread
 import traceback
 import time
 import RPi.GPIO as GPIO
@@ -10,13 +10,15 @@ class Runzonejob():
         self.num_secs = int(num_secs)
 
         try:
-            getconfig = configread()
+            getconfig = Configread()
             gpioid = getconfig.GPIO(self.zone)
             GPIO.setmode(GPIO.BCM)
             GPIO.setwarnings(False)
             GPIO.cleanup()
+            print("Zone {} job kicked off".format(self.zone))
             GPIO.setup(gpioid, GPIO.OUT)
             time.sleep(self.num_secs)
+            print("Zone {} job done".format(self.zone))
 
         except Exception as ex:
             traceback.print_exc()
