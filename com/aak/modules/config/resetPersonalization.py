89a019c71ebc53508c1f9e502b83f42e5caee3aa
krishnareddy <tkcreddy@yahoo.com>
2019-02-15 12:50:53 -0800

    Added new default user and password reset module.

--- com/aak/modules/config/resetPersonalization.py
+++ com/aak/modules/config/resetPersonalization.py
@@ -1,5 +1,8 @@
-from com.aak.modules.db.resetDb import Schedreset,Personalreset,Zonereset
+from com.aak.modules.db.resetDb import Schedreset,Personalreset,Zonereset,Userreset
+from com.aak.modules.db.userDA import Userscurd
 Schedreset()
 Personalreset()
 Zonereset()
-
+Userreset()
+users = Userscurd()
+users.insertUser("admin","password")
