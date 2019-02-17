27989ef8822403d16b9453f70d20f467f8926f10
krishnareddy <tkcreddy@yahoo.com>
2019-02-17 00:03:15 -0800

    python requirements file.

--- com/aak/modules/db/userDA.py
+++ com/aak/modules/db/userDA.py
@@ -1,12 +1,10 @@
 from com.aak.modules.db.dbDao import Dbdao,Users, Base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import exc
-from sqlalchemy import exc
+from sqlalchemy.orm import sessionmaker,exc
+from sqlalchemy.exc import IntegrityError as sqlerr
 from passlib.hash import pbkdf2_sha256
-from sqlite3 import IntegrityError
-
-import json
+from sqlite3 import IntegrityError as sqerr
 import traceback
+
 
 class Userscurd(object):
     def __init__(self):
@@ -21,67 +19,98 @@ class Userscurd(object):
 
     def getUsername(self,username):
         try:
-            session = self.DBSession()
+            self.session = self.DBSession()
             self.username = ''
-            self.userdata = session.query(Users).filter(Users.username == username).one()
+            self.userdata = self.session.query(Users).filter(Users.username == username).one()
             self.username = self.userdata.username
             return self.username
         except exc.NoResultFound:
             print("No rows")
 
         finally:
-            session.close()
+            self.session.close()
 
 
 
     def insertUser(self,username,password):
-        try:
-            self.username = username
-            #self.password = password
-            session = self.DBSession()
-            self.hash = pbkdf2_sha256.encrypt(password, rounds=200000, salt_size=16)
-            self.newuserdata = Users(username=self.username,password=self.hash)
-            session.add(self.newuserdata)
 
-        except exc.IntegrityError:
-            "print User is already there"
-        except IntegrityError:
-            "print User is already there"
+        self.username = username
+        self.session = self.DBSession()
+        self.hash = pbkdf2_sha256.encrypt(password, rounds=200000, salt_size=16)
+        self.newuserdata = Users(username=self.username,password=self.hash)
+        self.session.add(self.newuserdata)
+        try:
+            self.session.commit()
+            self.rv = True
+
+        except sqerr as e:
+            self.session.rollback()
+            print("print User is already there sqerr", e)
+            self.rv = False
+        except sqlerr as err:
+            self.session.rollback()
+            if "UNIQUE constraint failed: self.username" in str(err):
+                return False, "error, username already exists (%s)" % self.username
+            print("print User is already there exc", err)
+            self.rv = False
+
+
         finally:
-            session.commit()
-            session.close()
+
+            self.session.close()
+            #print(self.rv)
+            return self.rv
 
     def checkAuthentication(self,username,password):
         try:
-            session = self.DBSession()
-            self.hash = pbkdf2_sha256.encrypt(password, rounds=200000, salt_size=16)
-            self.userdata = session.query(Users).filter(Users.username == username).one()
+            self.session = self.DBSession()
+            self.userdata = self.session.query(Users).filter(Users.username == username).one()
             return pbkdf2_sha256.verify(password, self.userdata.password)
 
         except exc.NoResultFound:
             print("No user found.")
         finally:
-            session.close()
+            self.session.close()
 
 
 
     def updatepassword(self,username,password,newpassword):
         try:
             if self.checkAuthentication(username,password):
-                session = self.DBSession()
+                self.session = self.DBSession()
                 self.hash = pbkdf2_sha256.encrypt(newpassword, rounds=200000, salt_size=16)
-                self.userdata = session.query(Users).filter(Users.username == username).one()
+                self.userdata = self.session.query(Users).filter(Users.username == username).one()
                 self.userdata.password = self.hash
                 self.returnvalue = 1
             else:
                 self.returnvalue = 2
-
-
         except exc.NoResultFound:
             #traceback.print_exc()
             print("something failed")
             #session.add(new_zone)
         finally:
-            session.commit()
-            session.close()
+            self.session.commit()
+            self.session.close()
             return self.returnvalue
+
+
+    def deleteUsername(self,username):
+        try:
+            self.session = self.DBSession()
+            self.username = ''
+            self.session.query(Users).filter(Users.username == username).delete()
+            self.rv = True
+
+            # User.query.filter_by(id=123).delete()
+            # User.query.filter(User.id == 123).delete()
+            # self.username = self.userdata.username
+            # return self.username
+        except exc.NoResultFound:
+            print("No rows")
+            self.rv = False
+
+
+        finally:
+            self.session.commit()
+            self.session.close()
+            return self.rv
--- com/aak/modules/services/userAuthenticationcheck.py
+++ com/aak/modules/services/userAuthenticationcheck.py
@@ -2,18 +2,36 @@ from com.aak.modules.db.userDA import Userscurd
 
 
 x=Userscurd()
-#x.insertUser("admin1","password")
-if x.checkAuthentication("admin","password"):
-    print("print authentication is successfull")
+if x.insertUser("admin1","password"):
+   print("admin user was added")
 else:
-    print(" not valid Passsword")
+  print("failed user")
 
 
-#if x.updatepassword("admin","newpassword1","password"):
-#   print("password change is successfull")
+# if x.checkAuthentication("admin","password"):
+#     print("print authentication is successfull")
+# else:
+#     print(" not valid Passsword")
+
+#x.checkAuthentication("admin","password")
+
+
+
+# if x.updatepassword("admin","password","password1"):
+#    print("password change is successfull")
 #hash = pbkdf2_sha256.encrypt("password", rounds=200000, salt_size=16)
 
 
 #print(pbkdf2_sha256.verify("password", hash))
 
 #print(x.checkAuthentication("admin","password12"))
+
+# if x.deleteUsername("admin1"):
+#     print("admin user was deleted")
+# else:
+#   print("failed user")
+
+
+#x.insertUser("admin1","password")
+
+# x.deleteUsername("admin1")
