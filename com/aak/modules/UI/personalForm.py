eb309ced9d13cb1c0e41a2ff513911216d87d16f
krishnareddy <tkcreddy@yahoo.com>
2019-03-02 23:02:23 -0800

    Form to update profile

--- com/aak/modules/UI/personalForm.py
+++ com/aak/modules/UI/personalForm.py
@@ -1,15 +1,15 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm,Form
 from wtforms import StringField,TextField, IntegerField, TextAreaField, SubmitField, RadioField,SelectField
 
 from wtforms import validators, ValidationError
 
 
-class ContactForm(Form):
-    name = StringField("Name", [validators.DataRequired("Please enteryour name.")])
-    email = StringField("Email", [validators.DataRequired("Please enter your email address."),
+class PersonalForm(FlaskForm):
+    name = StringField("name", [validators.DataRequired("Please enteryour name.")])
+    email = StringField("email", [validators.DataRequired("Please enter your email address."),
                                 validators.Email("Please enter your email address.")])
-    Zip = IntegerField("zip", [validators.DataRequired("Please enter Zip Code."), validators.length(max=2)])
-    Country = StringField('Country', [validators.DataRequired("Please enter Country Code."),
+    zip = IntegerField("zip", [validators.DataRequired("Please enter Zip Code."), validators.length(max=6)])
+    country = StringField('country', [validators.DataRequired("Please enter Country Code."),
                                 validators.length(max=2)])
-    owm_appid = TextAreaField("owm_appid")
-    submit = SubmitField("Send")
+    owm_appid = StringField("owm_appid")
+    submit = SubmitField("Save")
