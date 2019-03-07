f589f299b8cac2a363711713baf54f46542d70d4
krishnareddy <tkcreddy@yahoo.com>
2019-03-06 23:16:24 -0800

    updated fields

--- com/aak/modules/UI/zoneForm.py
+++ com/aak/modules/UI/zoneForm.py
@@ -1,16 +1,10 @@
 from flask_wtf import FlaskForm,Form
-from wtforms import StringField,TextField, IntegerField, TextAreaField, SubmitField, RadioField,SelectField
+from wtforms import StringField,TextField, IntegerField, TextAreaField, SubmitField, RadioField,SelectField,HiddenField
 
 from wtforms import validators, ValidationError
 
 
 class ZoneForm(FlaskForm):
-    zone1 = StringField("zone1")
-    zone2 = StringField("zone2")
-    zone3 = StringField("zone3")
-    zone4 = StringField("zone4")
-    zone5 = StringField("zone5")
-    zone6 = StringField("zone6")
-    zone7 = StringField("zone7")
-    zone8 = StringField("zone8")
+    id = HiddenField("id")
+    name =StringField("name")
     submit = SubmitField("Save")
